949ac6ecd5e0654b48bc11d630ba3b5285566f28
ankit <iankitkhatrani@gmail.com>
2024-03-13 15:33:05 +0530

    add fix issue

--- src/component/KycManagement/index.jsx
+++ src/component/KycManagement/index.jsx
@@ -9,7 +9,7 @@ function KycManagement({ status }) {
     <div className="w-full rounded-lg bg-white px-[24px] py-[20px] dark:bg-darkblack-600">
       <div className="flex flex-col space-y-5">
       <h3 className="text-2xl font-bold pb-5 text-bgray-900 dark:text-white dark:border-darkblack-400 border-b border-bgray-200"> 
-        {status == "Pendding"?"KYC - Rejected Profiles":(status == "Rejected")?"KYC - Rejected Profiles":"KYC - Approved Profiles"}
+        {status == "Pendding"?"KYC - Pending Profiles":(status == "Rejected")?"KYC - Rejected Profiles":"KYC - Approved Profiles"}
       </h3>
 
         <PlayerTab status={status} />
